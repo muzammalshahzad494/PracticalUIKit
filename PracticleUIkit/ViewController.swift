b8fc325047208aa02639cf57212a49f1cfc3c152
Muzammal_09 <muzammal_09@Muzammals-MBP.local>
2024-08-19 22:21:06 +0500

    first commit

---
+++ PracticleUIkit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticleUIkit
+//
+//  Created by Muzammal_09 on 19/08/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
